183ed8359a2950f1e8eda21b590fbd523603cdb8
Orivelton Cesar <orivelton10@gmail.com>
2021-05-11 01:44:58 +0100

    fix: SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ReactElement } from 'react'
 import '../styles/global.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps): ReactElement {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Register Payment Methods</title>
+        <meta name="description" content="Register Payment Methods"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
